7b26d59ba131448f30a45b865b7317162ec90360
Duy631010 <nguyenkhanhduy10102006@gmail.com>
2026-05-01 18:19:43 +0700

    fix: remove title from sub-pages to hide them from sidebar

--- src/router/routes/cultural/subject.ts
+++ src/router/routes/cultural/subject.ts
@@ -10,24 +10,24 @@ export const subjectRoute: RouteRecordRaw[] = [
     path: 'subject/:id',
     name: 'cultural-subject-detail',
     component: () => import('../../../components/pages/SubjectDetailPage.vue'),
-    meta: { title: 'Chi tiết Môn học văn hóa', hideInMenu: true }
+    meta: { hideInMenu: true }
   },
   {
     path: 'subject/create',
     name: 'cultural-subject-create',
     component: () => import('../../../components/pages/CreateSubjectPage.vue'),
-    meta: { title: 'Thêm mới Môn học văn hóa', hideInMenu: true }
+    meta: { hideInMenu: true }
   },
   {
     path: 'subject/edit/:id',
     name: 'cultural-subject-edit',
     component: () => import('../../../components/pages/EditSubjectPage.vue'),
-    meta: { title: 'Điều chỉnh Môn học văn hóa', hideInMenu: true }
+    meta: { hideInMenu: true }
   },
   {
     path: 'subject/deleted',
     name: 'cultural-subject-deleted',
     component: () => import('../../../components/pages/DeletedSubjectPage.vue'),
-    meta: { title: 'Danh sách Môn học văn hóa đã xóa', hideInMenu: true }
+    meta: { hideInMenu: true }
   }
 ]
