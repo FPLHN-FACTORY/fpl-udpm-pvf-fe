89fdecb18c9d8d6cb1b8acf93bf69d0372a2b712
NgoVietHoang21 <nt0130207458@gmail.com>
2026-05-09 17:55:07 +0700

    Ready for Vercel deployment - fix build errors and add deployment configs

---
+++ src/components/pages/student/equipment/Detail.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Equipment Detail Page - Coming Soon</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: Implement equipment detail page
+</script>
---
+++ src/components/pages/student/equipment/Edit.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Equipment Edit Page - Coming Soon</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: Implement equipment edit page
+</script>
